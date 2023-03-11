0895994433c3a25383623f1c9344522d0bf40f48
Ryota Yamada <riita@amazon.co.jp>
2023-03-11 23:24:46 +0900

    rename from main.py tocmd.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
         version='1.0.0',
         install_requires=['PyYAML==6.0', 'click==8.1.3', 'boto3==1.26.71'],
         entry_points={
-            "console_scripts": ['mdt = main:__main__']
+            "console_scripts": ['mdt = cmd:__main__']
         }
 )

96beb6c6daf50127fe796301411bc17bc91cd5c1
Ryota Yamada <riita@amazon.co.jp>
2023-03-16 21:42:37 +0900

    fix: 🐛 module name to mdt

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 
 setup(
-        name='md-translate',
+        name='mdt',
         version='1.0.0',
+        py_modules=['mdt']
         install_requires=['PyYAML==6.0', 'click==8.1.3', 'boto3==1.26.71'],
         entry_points={
-            "console_scripts": ['mdt = cmd:main']
+            "console_scripts": ['mdt = mdt:run']
         }
 )

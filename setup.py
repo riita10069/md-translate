04a3944f337e35499c6f9b1cf67a514ed7233c2c
Ryota Yamada <riita@amazon.co.jp>
2023-03-16 21:45:09 +0900

    fix: 🐛 syntax, forget comma

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
         name='mdt',
         version='1.0.0',
-        py_modules=['mdt']
+        py_modules=['mdt'],
         install_requires=['PyYAML==6.0', 'click==8.1.3', 'boto3==1.26.71'],
         entry_points={
             "console_scripts": ['mdt = mdt:run']

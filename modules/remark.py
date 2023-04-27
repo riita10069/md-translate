04b34eee4374a5dd0702927e7358bcf16443705a
Ryota Yamada <riita@amazon.co.jp>
2023-04-27 17:42:07 +0900

    feat: 🎸 add double quotation to filename

--- modules/remark.py
+++ modules/remark.py
@@ -2,8 +2,10 @@ import subprocess
 
 
 def mdToJson(filename):
+    filename = '"'+filename+'"'
     subprocess.check_call('remark --use remark-directive --tree-out < ' + filename + '> output.json', shell=True)
 
 
 def jsonToMd(filename):
+    filename = '"'+filename+'"'
     subprocess.check_call('remark -u remark-directive --tree-in < output.json > ' + filename, shell=True)

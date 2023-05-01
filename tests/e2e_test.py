a0179e62003357adc82e2ecf6bead707271f7d6e
Yuriko Takahashi <yurikooo@amazon.co.jp>
2023-05-01 16:47:03 +0900

    fix ci

--- tests/e2e_test.py
+++ tests/e2e_test.py
@@ -84,49 +84,49 @@ Hello, world!
 
 def test_path_option_1(e2e_test):
     print('specify file path / from en to ja')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample1.md', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample1.md', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')
 
 
 def test_path_option_2(e2e_test):
     print('specify file path / from en (file path contains "en") to ja')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample2.en.md', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample2.en.md', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample2.ja.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample2.ja.md')
 
 
 def test_path_option_3(e2e_test):
     print('specify file path / from ja to en')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample3.ja.md --from ja --to en', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample3.ja.md --from ja --to en', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.md')
 
 
 def test_path_option_4(e2e_test):
     print('specify file path / from ja to zh')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample3.ja.md --from ja --to zh', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample3.ja.md --from ja --to zh', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.zh.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.zh.md')
 
 
 def test_path_option_5(e2e_test):
     print('specify file path with output directory / from en to ja')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample1.md --output {TEST_DIR}/{OUTPUT_DIR}', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample1.md --output {TEST_DIR}/{OUTPUT_DIR}', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{OUTPUT_DIR}/sample1.md')
     os.remove(f'{TEST_DIR}/{OUTPUT_DIR}/sample1.md')
 
 
 def test_path_option_6(e2e_test):
     print('specify file path with output directory / from ja to en')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample3.ja.md --output {TEST_DIR}/{OUTPUT_DIR} --from ja --to en', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample3.ja.md --output {TEST_DIR}/{OUTPUT_DIR} --from ja --to en', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{OUTPUT_DIR}/sample3.md')
     os.remove(f'{TEST_DIR}/{OUTPUT_DIR}/sample3.md')
 
 
 def test_path_option_7(e2e_test):
     print('specify directory path / from en to ja')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample2.ja.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')
@@ -135,14 +135,14 @@ def test_path_option_7(e2e_test):
 
 def test_path_option_8(e2e_test):
     print('specify directory path / from ja to en')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR} --from ja --to en', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR} --from ja --to en', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.md')
 
 
 def test_path_option_9(e2e_test):
     print('specify directory path with -r option / from en to ja')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR} -r', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR} -r', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample2.ja.md')
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/{NESTED_DIR}/sample4.ja.md')
@@ -153,7 +153,7 @@ def test_path_option_9(e2e_test):
 
 def test_path_option_10(e2e_test):
     print('specify directory path with -r option / from ja to en')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR} -r --from ja --to en', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR} -r --from ja --to en', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.md')
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/{NESTED_DIR}/sample5.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample3.md')
@@ -162,7 +162,7 @@ def test_path_option_10(e2e_test):
 
 def test_path_option_11(e2e_test):
     print('specify directory path with output directory / from en to ja')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR} --output {TEST_DIR}/{OUTPUT_DIR}', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR} --output {TEST_DIR}/{OUTPUT_DIR}', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{OUTPUT_DIR}/sample1.md')
     assert os.path.exists(f'{TEST_DIR}/{OUTPUT_DIR}/sample2.en.md')
     os.remove(f'{TEST_DIR}/{OUTPUT_DIR}/sample1.md')
@@ -171,20 +171,20 @@ def test_path_option_11(e2e_test):
 
 def test_path_option_12(e2e_test):
     print('specify file path contains space / from en to ja')
-    subprocess.run(f'python3 mdt.py --path "{TEST_DIR}/{CASE_DIR}/filename with space.md"', shell=True)
+    subprocess.run(f'mdt --path "{TEST_DIR}/{CASE_DIR}/filename with space.md"', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{CASE_DIR}/filename with space.ja.md')
     os.remove(f'{TEST_DIR}/{CASE_DIR}/filename with space.ja.md')
 
 
 def test_no_hugo_option(e2e_test):
     print('use --no-hugo options and translate normal markdown file')
-    subprocess.run(f'python3 mdt.py --path README.md --no-hugo', shell=True)
+    subprocess.run(f'mdt --path README.md --no-hugo', shell=True)
     assert os.path.exists(f'README.ja.md')
     os.remove(f'README.ja.md')
 
 
 def test_deepl_option(e2e_test):
     print('use --deepl-free option')
-    subprocess.run(f'python3 mdt.py --path {TEST_DIR}/{SAMPLE_DIR}/sample1.md --deepl-free', shell=True)
+    subprocess.run(f'mdt --path {TEST_DIR}/{SAMPLE_DIR}/sample1.md --deepl-free', shell=True)
     assert os.path.exists(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')
     os.remove(f'{TEST_DIR}/{SAMPLE_DIR}/sample1.ja.md')

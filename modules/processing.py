b39990c75e745e061b026de660cec725150a2fe3
Ryota Yamada <riita@amazon.co.jp>
2023-05-26 03:25:19 +0900

    feat: 🎸 Support HTML Tag

--- modules/processing.py
+++ modules/processing.py
@@ -45,13 +45,13 @@ def merge(before, lookup_table):
 
     after = []
     for leaf in before:
-        if leaf[const.TYPE_TYPE] in [const.TYPE_STRONG, const.TYPE_EMPHASIS, const.TYPE_INLINE_CODE, const.TYPE_LINK, const.TYPE_HTML]:
+        if leaf[const.TYPE_TYPE] in [const.TYPE_STRONG, const.TYPE_EMPHASIS, const.TYPE_INLINE_CODE, const.TYPE_LINK]:
 
             id = "http://no-translate-" + next_alphabet(lookup_table['current_alphabet']) + ".dev"
             lookup_table['current_alphabet'] = next_alphabet(lookup_table['current_alphabet'])
             lookup_table["ids"].append(id)
 
-            if leaf[const.TYPE_TYPE] in [const.TYPE_INLINE_CODE, const.TYPE_HTML]:
+            if leaf[const.TYPE_TYPE] in [const.TYPE_INLINE_CODE]:
                 lookup_table[id] = {
                     "leaf_types": [leaf[const.TYPE_TYPE]],
                     "leaf_value": leaf[const.VALUE_TYPE]
@@ -83,7 +83,7 @@ def merge(before, lookup_table):
 
             ret += id
 
-        elif leaf[const.TYPE_TYPE] == const.TYPE_TEXT:
+        elif leaf[const.TYPE_TYPE] in [const.TYPE_TEXT, const.TYPE_HTML]:
             ret += leaf[const.VALUE_TYPE]
         else:
             if ret != "":
@@ -103,22 +103,132 @@ def merge(before, lookup_table):
     return after, ret
 
 
+def distinguish_html_tag(input_string):
+    if input_string.startswith("<!--") and input_string.endswith("-->"):
+        return const.HTML_TAG_TYPE_COMMENT
+    elif input_string.startswith("<") and input_string.endswith("/>"):
+        return const.HTML_TAG_TYPE_SELF_CLOSING_TAG
+    elif input_string.startswith("</") and input_string.endswith(">"):
+        return const.HTML_TAG_TYPE_CLOSING_TAG
+    elif input_string.startswith("<") and input_string.endswith(">"):
+        return const.HTML_TAG_TYPE_BEGINNING_TAG
+    else:
+        print("以下の想定外のHTMLタグが含まれています。管理者へ連絡してください。")
+        print(input_string)
+        return ""
+
+
+def html_merge(before, lookup_table):
+    beginning_tags = []
+    is_in_html = False
+
+    html_text = ""
+    after = []
+
+    for node in before:
+        if is_in_html:
+            if node[const.TYPE_TYPE] in [const.TYPE_STRONG]:
+                for strong_children in node[const.CHILDREN_TYPE]:
+                    if strong_children[const.TYPE_TYPE] in [const.TYPE_TEXT]:
+                        html_text += make_strong(strong_children[const.VALUE_TYPE])
+            if node[const.TYPE_TYPE] in [const.TYPE_EMPHASIS]:
+                for emphasis_children in node[const.CHILDREN_TYPE]:
+                    if emphasis_children[const.TYPE_TYPE] in [const.TYPE_TEXT]:
+                        html_text += make_emphasis(emphasis_children[const.VALUE_TYPE])
+            if node[const.TYPE_TYPE] in [const.TYPE_TEXT, const.TYPE_HTML, const.TYPE_INLINE_CODE]:
+                if node[const.TYPE_TYPE] in [const.TYPE_TEXT, const.TYPE_HTML]:
+                    html_text += node[const.VALUE_TYPE]
+                elif node[const.TYPE_TYPE] in [const.TYPE_INLINE_CODE]:
+                    html_text += make_inlinecode(node[const.VALUE_TYPE])
+
+        if node[const.TYPE_TYPE] == const.TYPE_HTML:
+            tag_type = distinguish_html_tag(node[const.VALUE_TYPE])
+            if tag_type == const.HTML_TAG_TYPE_BEGINNING_TAG:
+                beginning_tags.append(node)
+                is_in_html = True
+                html_text = node[const.VALUE_TYPE]
+            elif tag_type == const.HTML_TAG_TYPE_CLOSING_TAG:
+                beginning_tags.pop()
+                if len(beginning_tags) == 0:
+                    id = "http://no-translate-" + next_alphabet(lookup_table['current_alphabet']) + ".dev"
+                    lookup_table['current_alphabet'] = next_alphabet(lookup_table['current_alphabet'])
+                    lookup_table["ids"].append(id)
+
+                    lookup_table[id] = {
+                        "leaf_types": [const.TYPE_HTML],
+                        "leaf_value": html_text
+                    }
+
+                    is_in_html = False
+                    after.append({
+                        "type": const.TYPE_HTML,
+                        "value": id
+                    })
+                    html_text = ""
+            elif tag_type in [const.HTML_TAG_TYPE_SELF_CLOSING_TAG, const.HTML_TAG_TYPE_COMMENT] and not is_in_html:
+                id = "http://no-translate-" + next_alphabet(lookup_table['current_alphabet']) + ".dev"
+                lookup_table['current_alphabet'] = next_alphabet(lookup_table['current_alphabet'])
+                lookup_table["ids"].append(id)
+
+                lookup_table[id] = {
+                    "leaf_types": [const.TYPE_HTML],
+                    "leaf_value": node[const.VALUE_TYPE]
+                }
+
+                after.append({
+                    "type": const.TYPE_HTML,
+                    "value": id
+                })
+
+        elif not is_in_html:
+            if html_text != "":
+                after.append({
+                    "type": const.TYPE_TEXT,
+                    "value": html_text,
+                    "position": before[0][const.POSITION_TYPE]
+                })
+                html_text = ""
+            after.append(node)
+
+    if html_text != "":
+        after.append({
+            "type": const.TYPE_TEXT,
+            "value": html_text
+        })
+    return after, html_text
+
+
+def html_dfs(ast, lookup_table):
+    root = ast[const.CHILDREN_TYPE]
+
+    if isinstance(root, list):
+        for node in root:
+            if const.CHILDREN_TYPE in node:
+                html_dfs(node, lookup_table)
+            if node[const.TYPE_TYPE] == const.TYPE_HTML:
+                new_node, _ = html_merge(root, lookup_table)
+                ast[const.CHILDREN_TYPE] = new_node
+
+
 def dfs(ast, lookup_table):
     root = ast[const.CHILDREN_TYPE]
     if isinstance(root, list):
         for node in root:
             if node[const.TYPE_TYPE] in [const.TYPE_STRONG, const.TYPE_EMPHASIS, const.TYPE_INLINE_CODE,
-                                         const.TYPE_LINK]:
-                new_node, _ = merge(root, lookup_table)
-                ast[const.CHILDREN_TYPE] = new_node
-                break
+                                         const.TYPE_LINK, const.TYPE_HTML]:
+                if node[const.TYPE_TYPE] != const.TYPE_HTML or ast[const.TYPE_TYPE] == const.TYPE_PARAGRAPH:
+                    new_node, _ = merge(root, lookup_table)
+                    ast[const.CHILDREN_TYPE] = new_node
+                    break
             if const.CHILDREN_TYPE in node:
                 dfs(node, lookup_table)
     elif isinstance(root, dict):
         node = root
-        if node[const.TYPE_TYPE] in [const.TYPE_STRONG, const.TYPE_EMPHASIS, const.TYPE_INLINE_CODE, const.TYPE_LINK]:
-            new_node, _ = merge([root], lookup_table)
-            ast[const.CHILDREN_TYPE] = new_node
+        if node[const.TYPE_TYPE] in [const.TYPE_STRONG, const.TYPE_EMPHASIS, const.TYPE_INLINE_CODE, const.TYPE_LINK,
+                                     const.TYPE_HTML]:
+            if node[const.TYPE_TYPE] != const.TYPE_HTML or ast[const.TYPE_TYPE] == const.TYPE_PARAGRAPH:
+                new_node, _ = merge([root], lookup_table)
+                ast[const.CHILDREN_TYPE] = new_node
         if const.CHILDREN_TYPE in node:
             dfs(node, lookup_table)
 
@@ -166,6 +276,7 @@ def jsonProcessingBeforeTranslation():
     lookup_table = {"current_alphabet": '', "ids": []}
     json_open = open('output.json', 'r', encoding='utf-8')
     ast = json.load(json_open)
+    html_dfs(ast, lookup_table)
     dfs(ast, lookup_table)
 
     return ast, lookup_table
@@ -187,7 +298,8 @@ def make_link(content, url, src_lang, dest_lang, deepl_free, deepl_pro):
     return "[" + translate.translate(content, src_lang, dest_lang, deepl_free, deepl_pro) + "]" + "(" + url + ")"
 
 
-def mdProcessingAfterTranslation(dest_file_path, src_lang, dest_lang, deepl_free, deepl_pro, translated_header_yaml_data, lookup_table):
+def mdProcessingAfterTranslation(dest_file_path, src_lang, dest_lang, deepl_free, deepl_pro,
+                                 translated_header_yaml_data, lookup_table):
     # add translated hugo header
     dest_md_file = open(dest_file_path, 'r', encoding='utf-8')
     translated_md_content_lines = dest_md_file.readlines()
@@ -213,7 +325,8 @@ def mdProcessingAfterTranslation(dest_file_path, src_lang, dest_lang, deepl_free
                     elif leaf_type == const.TYPE_EMPHASIS:
                         attribute = make_emphasis(leaf_data["leaf_value"])
                     elif leaf_type == const.TYPE_LINK:
-                        attribute = make_link(leaf_data["leaf_value"], leaf_data["leaf_url"], src_lang, dest_lang, deepl_free, deepl_pro)
+                        attribute = make_link(leaf_data["leaf_value"], leaf_data["leaf_url"], src_lang, dest_lang,
+                                              deepl_free, deepl_pro)
                     elif leaf_type == const.TYPE_HTML:
                         attribute = leaf_data["leaf_value"]
                     processed_line = processed_line.replace(id, attribute)

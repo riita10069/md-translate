77d586f3505abc38fcb2fe3c8494665af8e220ad
Yuriko Takahashi <yurikooo@amazon.co.jp>
2023-05-25 17:48:09 +0900

    fix html tags bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='mdt',
-        version='1.0.4',
+        version='1.0.5',
         py_modules=['mdt'],
         packages=['modules'],
         license='MIT',

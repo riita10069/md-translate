a24ab9d9ca12b349a60d631304eda5721212d348
Ryota Yamada <riita@amazon.co.jp>
2023-06-16 17:48:05 +0900

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='mdt',
-        version='1.0.6',
+        version='1.0.1',
         py_modules=['mdt'],
         packages=['modules'],
         license='MIT',

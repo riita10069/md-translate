9f3bc34ddbbd4a77d4cf6a03f98557fd62bb4048
Ryota Yamada <riita@amazon.co.jp>
2023-06-16 17:43:27 +0900

    docs: ✏️ change setup.pu

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
         name='mdt',
-        version='1.0.7',
+        version='1.0.6',
         py_modules=['mdt'],
         packages=['modules'],
         license='MIT',

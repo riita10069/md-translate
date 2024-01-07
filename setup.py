cd9f3445be7fdf10f4c70a9abfda2decc0f24ca6
Ryota Yamada <riita@amazon.co.jp>
2024-01-08 00:04:04 +0900

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='mdt',
-        version='2.0.0',
+        version='2.0.1',
         py_modules=['mdt'],
         packages=['modules'],
         license='MIT',

41d9234ee3557451478ccbb05b8853603091ffb8
Ryota Yamada <riita@amazon.co.jp>
2024-01-11 17:16:09 +0900

    replace translate_text

--- modules/translate_by_claude.py
+++ modules/translate_by_claude.py
@@ -3,6 +3,7 @@ import json
 from bs4 import BeautifulSoup
 from botocore.config import Config
 from modules import prompt
+import unicodedata
 
 bedrock_runtime_client = boto3.client(service_name='bedrock-runtime',
                                       region_name='us-west-2',
@@ -27,8 +28,32 @@ def translate_by_claude(content):
     soup = BeautifulSoup(response_body, 'html.parser')
 
     translated_text = str(soup.find('translated'))
-    return (translated_text.replace("<translated>", "").replace("</translated>", "").
-            replace("<no-translate>", "").replace("</no-translate>", "").strip())
+    translated_text = (translated_text.replace('re:Post', 're\:Post').
+                       replace('re:post', 're\:post').
+                       replace('re:Invent', 're\:Invent').
+                       replace('re:invent', 're\:invent').
+                       replace('&amp;', '&').
+                       replace('&gt;', '>').
+                       replace('&lt;', '<').
+                       replace("<translated>", "").
+                       replace("</translated>", "").
+                       replace("<no-translate>", "").
+                       replace("</no-translate>", "").strip())
+    translated_text = insert_space_between_full_and_half_width_chars(translated_text)
+
+    return translated_text
+
+def insert_space_between_full_and_half_width_chars(text):
+    new_text = ""
+    prev_char = None
+    for char in text:
+        if prev_char is not None and prev_char != " " and char != " ":
+            if ((unicodedata.east_asian_width(prev_char) in 'FWA' and unicodedata.east_asian_width(char) not in 'FWA') or
+                (unicodedata.east_asian_width(prev_char) not in 'FWA' and unicodedata.east_asian_width(char) in 'FWA')):
+                new_text += " "
+        new_text += char
+        prev_char = char
+    return new_text
 
 def translate_by_claude_for_hugo_front_matter(content):
     body = json.dumps({
